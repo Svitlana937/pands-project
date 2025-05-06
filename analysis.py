21eba19e7adbf5f86c4850c30ba995eedf83b827
unknown <skrasunya1@gmail.com>
2025-05-06 18:19:14 +0100

    got access to all columns

--- analysis.py
+++ analysis.py
@@ -10,12 +10,31 @@ with open('iris/iris.data', 'r') as f:
 lines = read_data.strip().split('\n')
 
 
-# Extracting the first column without comprehension
+# Extracting the first column 
 first_column = []
 for line in lines:
     if line.strip():  # Skip empty lines
         first_column.append(line.split(',')[0])
 
-# Printing the first column
-print(first_column)
+
+# Extracting the second column 
+second_column = []
+for line in lines:
+    if line.strip():  # Skip empty lines
+        second_column.append(line.split(',')[1])
+
+
+# Extracting the third column 
+third_column = []
+for line in lines:
+    if line.strip():  # Skip empty lines
+        third_column.append(line.split(',')[2])
+
+
+# Extracting the fourth column
+fourth_column = []
+for line in lines:
+    if line.strip():  # Skip empty lines
+        fourth_column.append(line.split(',')[3])
+
 
