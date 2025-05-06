4d13c0336bedffb36e5a21f93774ff4f3cec3665
unknown <skrasunya1@gmail.com>
2025-05-06 18:07:38 +0100

    getting an access to the data file

--- analysis.py
+++ analysis.py
@@ -1,1 +1,7 @@
-print()
+# Getting an access to iris dataset
+# https://docs.python.org/3/tutorial/inputoutput.html
+with open('iris/iris.data', 'r') as f:
+    read_data = f.read()
+
+# Checking if we got the access to the file
+# print(read_data)    
